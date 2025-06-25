6ce9187d1620f486c041dd5940e21d98c6f9d53e
Contzo <ilierazvan1497@outlook.com>
2025-06-25 15:30:52 +0300

    Created the deposit collateral

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 1. Relative stability - anchored or pegged to the $1.00
    - Chianlink price feed.
    - Set a function to exchange ETCH $ BTC for USD
-2. Stability Mechanism (minitng) - algorithmic (Decentralize).
+2. Stability Mechanism (minting) - algorithmic (Decentralize).
    - People can mint stable coins only if they have enough collateral (determined by the code)
-3. Collater: Exogenous (Crypto currencies ETH&BTC).
+3. Collateral: Exogenous (Crypto currencies ETH&BTC).
    - wETH
    - wBTC
+4. I also want to implement a simple fee for the protocol and a simple interest system for the deposits and dept.
--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,32 +1,35 @@
 // SPDX-License-Identifier: MIT
 /**
  * - Inside a sol file contract elements should be laid like this:
-	1. Pragma statements
-	2. Import statements
-	3. Events
-	4. Errors
-	5. Interfaces
-	6. Libraries
-	7. Contracts
-- Inside each contract we have this order of declaration:
-	1. Type declaration
-	2. State variables
-	3. Events
-	4. Errors
-	5. Modifiers
-	6. Functions
-- Also functions inside a contract should be declared like this:
-	1. constructor
-	2. receive function (if exists)
-	3. fallback function (if exists)
-	4. external
-	5. public
-	6. internal
-	7. private
-	8. view & pure functions
+ * 	1. Pragma statements
+ * 	2. Import statements
+ * 	3. Events
+ * 	4. Errors
+ * 	5. Interfaces
+ * 	6. Libraries
+ * 	7. Contracts
+ * - Inside each contract we have this order of declaration:
+ * 	1. Type declaration
+ * 	2. State variables
+ * 	3. Events
+ * 	4. Errors
+ * 	5. Modifiers
+ * 	6. Functions
+ * - Also functions inside a contract should be declared like this:
+ * 	1. constructor
+ * 	2. receive function (if exists)
+ * 	3. fallback function (if exists)
+ * 	4. external
+ * 	5. public
+ * 	6. internal
+ * 	7. private
+ * 	8. view & pure functions
  */
 pragma solidity ^0.8.20;
+
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol"; 
 
 /**
  * @title DSCEngine
@@ -42,17 +45,25 @@ import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
  * @notice this contract is the core of the DSC system. It handles all the logic for minting and redeeming DSC, as well as depositing & withdrawing.
  * @notice This contract is Very loosely based on the MakerDAO (DAI) system
  */
-contract DSCEngine {
+contract DSCEngine is ReentrancyGuard {
     /*//////////////////////////////////////////////////////////////
                             STATE VARIABLES
     //////////////////////////////////////////////////////////////*/
     mapping(address tokenAddress => address priceFeed) private s_priceFeeds;
     DecentralizedStableCoin private immutable i_dscToken;
+    mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event CollateralDeposited(address indexed user, address indexed token, uint256 amount); 
     /*//////////////////////////////////////////////////////////////
                                  ERRORS
     //////////////////////////////////////////////////////////////*/
+
     error DSCEngine__NeedsMoreThenZero();
     error DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeTheSameLength();
+    error DSCEngine__TransferFailed(); 
     error DSCEngine__TokenAddressIsNotAllowed();
 
     /*//////////////////////////////////////////////////////////////
@@ -73,11 +84,7 @@ contract DSCEngine {
     /*//////////////////////////////////////////////////////////////
                                FUNCTIONS
     //////////////////////////////////////////////////////////////*/
-    constructor(
-        address[] memory _allowedTokenAddresses,
-        address[] memory _priceFeedsAddresses,
-        address _dscAddress
-    ) {
+    constructor(address[] memory _allowedTokenAddresses, address[] memory _priceFeedsAddresses, address _dscAddress) {
         if (_allowedTokenAddresses.length != _priceFeedsAddresses.length) {
             revert DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeTheSameLength();
         }
@@ -94,15 +101,22 @@ contract DSCEngine {
     function depositCollateralAndMintDsc() external {}
 
     /**
-     *
+     * @notice follows CEI pattern
      * @param _tokenCollateralAddress The address of the token to deposit as collateral
      * @param _amountCollateral  The amount of collateral to deposit
      */
-    function depositCollateral(
-        address _tokenCollateralAddress,
-        uint256 _amountCollateral
-    ) external moreThenZero(_amountCollateral) isAllowedToken(_tokenCollateralAddress) {
-
+    function depositCollateral(address _tokenCollateralAddress, uint256 _amountCollateral)
+        external
+        moreThenZero(_amountCollateral)
+        isAllowedToken(_tokenCollateralAddress)
+        nonReentrant
+    {
+        s_collateralDeposited[msg.sender][_tokenCollateralAddress] += _amountCollateral;
+        emit CollateralDeposited(msg.sender, _tokenCollateralAddress, _amountCollateral);
+        bool transferSuccess = IERC20(_tokenCollateralAddress).transferFrom(msg.sender, address(this), _amountCollateral);
+        if(!transferSuccess){
+            revert DSCEngine__TransferFailed() ; 
+        }
     }
 
     function redeemCollateral() external {}
